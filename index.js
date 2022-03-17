c0ffe47629e9f828b3bf65b6ad9cdaeb3cac454f
Ahmed AlAmiri <a.alamiri.96@gmail.com>
2022-03-18 02:03:37 +0300

    DO BMI Calculator challenge

--- index.js
+++ index.js
@@ -8,4 +8,44 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+
+  // calculate bmi
+  let heightInMeters = height / 100;
+  let bmi = (weight / (heightInMeters * heightInMeters)).toFixed(1);
+
+  // assign state based on bmi
+  let bState = "";
+  let maxUnder = 18.5;
+  let maxHealthy = 25.0;
+  let maxOver = 30.0;
+  if (bmi < maxUnder) bState = "Underweight";
+  else if (bmi < maxHealthy) bState = "Healthy Weight";
+  else if (bmi < maxOver) bState = "Overweight";
+  else bState = "Obesity";
+
+  // assign state based on bmi and age
+  let baState = "";
+  let incrementValue = 1;
+  if (age < 25) {
+  } else if (age < 35) {
+    incrementValue *= 2;
+  } else if (age < 45) {
+    incrementValue *= 2;
+  } else if (age < 55) {
+    incrementValue *= 3;
+  } else if (age < 65) {
+    incrementValue *= 4;
+  } else {
+    incrementValue *= 5;
+  }
+  maxUnder = 18 + incrementValue;
+  maxHealthy = 23.1 + incrementValue;
+  maxOver = 28.1 + incrementValue;
+  if (bmi < maxUnder) baState = "Underweight";
+  else if (bmi < maxHealthy) baState = "Healthy Weight";
+  else if (bmi < maxOver) baState = "Overweight";
+  else baState = "Obesity";
+
+  let alertMessage = `Your BMI is ${bmi}\nYour BMI state is ${bState}\nYour BMI based on your age is ${baState}`;
+  alert(alertMessage);
 }
